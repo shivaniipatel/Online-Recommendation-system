bfd3d0a4e9c0429817e3351c4e276cda9f2df2f5
Shivani <shivani.patel@leadschool.in>
2021-01-17 14:15:04 +0530

    added static url in url pattern

--- login/urls.py
+++ login/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
         path('register/', views.register, name = 'register'),
         path('book_list/', views.book_list, name='book_list'),
         #path('book_list/', views.content_rec, name='content_rec'),
-        ]
+        ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
